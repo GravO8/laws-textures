ff357d6db42efad0f09e38d1d5c6fe6c375cdb1a
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-07-13 18:10:44 +0100

    Remove the `input_dim` variable - it is now inferred by the input dim

--- feature_maps.py
+++ feature_maps.py
@@ -12,23 +12,21 @@ W - wave
 
 
 class FeatureMaps:
-    def __init__(self, basis_vectors: list, vector_dims: int, input_dim: int):
+    def __init__(self, basis_vectors: list, vector_dims: int):
         assert vector_dims > 1
-        assert input_dim > 0
         assert vector_dims % 2 == 1
         for v in basis_vectors:
-            assert v.size == 3
+            assert v.ndim == 3
         self.basis_vectors  = basis_vectors
         self.vector_dims    = vector_dims
-        self.input_dim      = input_dim
         self.vec_counter    = 0
-        self.generate_vector()
+        self.generate_vectors()
         
     def get_vector_name(self, j):
         self.vec_counter += 1
         return f"X{self.vec_counter}-" + str(self.vector_dims)
 
-    def generate_vector(self):
+    def generate_vectors(self):
         N            = 1 + (self.vector_dims - 3) // 2
         j            = 0
         combinations = list(combinations_with_replacement(range(len(self.basis_vectors)), N))
@@ -39,16 +37,21 @@ class FeatureMaps:
                 vector = np.convolve(vector, self.basis_vectors[comb[i]])
             self.vectors[self.get_vector_name(j)] = vector
             j += 1
+            
+    def get_features(self, x, filters: list, preprocess: bool = True):
+        maps = np.repeat(x[:, :, np.newaxis], , axis=2)
+        for dim in range(x.ndim):
+            ...
 
 
-def laws_textures(vector_dims: int = 5, input_dim: int = 2):
+def laws_textures(vector_dims: int = 5):
     basis_vectors = [
         np.array([ 1, 2, 1]),
         np.array([-1, 0, 1]),
         np.array([-1, 2,-1])
     ]
-    return FeatureMaps(basis_vectors, vector_dims, input_dim)
+    return FeatureMaps(basis_vectors, vector_dims)
 
 
 if __name__ == "__main__":
-    laws_textures(vector_dims = 7)
+    laws_textures(vector_dims = 3)
