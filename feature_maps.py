7b6f517e27b47e636c8b4a923b88bdb305248630
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-07-14 09:41:13 +0100

    Add `get_features` method

--- feature_maps.py
+++ feature_maps.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import combinations_with_replacement
+from scipy import signal
 
 VECTOR_INITIALS = ["L", "E", "S", "W", "R", "O"]
 '''
@@ -16,7 +17,7 @@ class FeatureMaps:
         assert vector_dims > 1
         assert vector_dims % 2 == 1
         for v in basis_vectors:
-            assert v.ndim == 3
+            assert (v.ndim == 1) and (len(v) == 3)
         self.basis_vectors  = basis_vectors
         self.vector_dims    = vector_dims
         self.vec_counter    = 0
@@ -30,19 +31,29 @@ class FeatureMaps:
         N            = 1 + (self.vector_dims - 3) // 2
         j            = 0
         combinations = list(combinations_with_replacement(range(len(self.basis_vectors)), N))
-        self.vectors = {}
+        # self.vectors = {}
+        self.vectors = []
         for comb in combinations:
             vector = self.basis_vectors[comb[0]]
             for i in range(1,len(comb)):
                 vector = np.convolve(vector, self.basis_vectors[comb[i]])
-            self.vectors[self.get_vector_name(j)] = vector
+            # self.vectors[self.get_vector_name(j)] = vector
+            self.vectors.append(vector)
             j += 1
             
-    def get_features(self, x, filters: list, preprocess: bool = True):
-        maps = np.repeat(x[:, :, np.newaxis], , axis=2)
-        for dim in range(x.ndim):
-            ...
-
+    def get_features(self, x, preprocess: bool = True, merge_symmetric: bool = True):
+        input_dim = x.ndim
+        kernels_i = list(combinations_with_replacement(range(self.vector_dims), input_dim))
+        kernels_i = kernels_i[1:] # remove the first non zero sum kernel
+        maps      = np.repeat(x[..., np.newaxis], len(kernels_i), axis = input_dim)
+        # if preprocess:
+            # TODO
+        for dim in range(input_dim):
+            shape = tuple([self.vector_dims if i == dim else 1 for i in range(input_dim)])
+            for kernel_i in kernels_i:
+                i = kernel_i[dim]
+                maps[..., dim] = signal.fftconvolve(maps[..., dim], self.vectors[i].reshape(shape), mode = "same")
+        return maps
 
 def laws_textures(vector_dims: int = 5):
     basis_vectors = [
@@ -54,4 +65,6 @@ def laws_textures(vector_dims: int = 5):
 
 
 if __name__ == "__main__":
-    laws_textures(vector_dims = 3)
+    laws = laws_textures(vector_dims = 3)
+    x = np.zeros((32,32))
+    laws.get_features(x)
