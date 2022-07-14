be7b1e6331f51b5dc9a3347f2246587c0a4ea33f
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-07-14 18:04:38 +0100

    Refactor `get_features` into methods

--- feature_maps.py
+++ feature_maps.py
@@ -36,21 +36,41 @@ class FeatureMaps(ABC):
     
     def get_kernel_shape(self, input_dim, dim):
         return tuple([self.vector_dims if i == dim else 1 for i in range(input_dim)])
-            
-    def get_features(self, x, window_size: int = 15, preprocess: bool = True, 
-        merge_symmetric: bool = True, compute_energy: bool = True):
-        input_dim = x.ndim
-        if preprocess:
-            smooth   = np.ones((window_size,)*input_dim)/(window_size**2)
-            x        = signal.fftconvolve(x, smooth, mode = "same")
+        
+    def preprocess_input(self, x, window_size, input_dim):
+        smooth   = np.ones((window_size,)*input_dim)/(window_size**2)
+        x        = signal.fftconvolve(x, smooth, mode = "same")
+        return x
+        
+    def merge_symmetric_maps(self, maps, permutations):
+        i = 0
+        j = 0
+        for permutation_set in permutations:
+            n_perm = len(permutation_set)
+            maps[...,j] = np.mean(maps[..., i:i+n_perm], axis = -1)
+            i += n_perm
+            j += 1
+        maps = maps[...,:j]
+        return maps
+        
+    def compute_energy_maps(self, maps, window_size, input_dim):
+        maps     = np.abs(maps)
+        abs_save = np.ones((window_size,)*input_dim) # absolute save
+        maps     = np.stack([signal.fftconvolve(maps[...,i], abs_save, mode = "valid") for i in range(maps.shape[-1])], axis = -1)
+        return maps
+        
+    def generate_kernel_permutations(self, input_dim):
         kernels_i    = list(itertools.combinations_with_replacement(range(len(self.vectors)), input_dim))
-        n_maps       = len(self.vectors)**(input_dim) -1
         permutations = []
         for kernel_i in kernels_i:
             permutations.append( list(set(itertools.permutations(kernel_i, input_dim))) )
         permutations.pop(0) # remove the first non zero sum kernel
-        maps = np.repeat(x[..., np.newaxis], n_maps, axis = input_dim)
-        j = 0
+        return permutations
+        
+    def compute_maps_separably(self, x, input_dim, permutations):
+        n_maps = len(self.vectors)**(input_dim) -1
+        maps   = np.repeat(x[..., np.newaxis], n_maps, axis = input_dim)
+        j      = 0
         for permutation_set in permutations:
             for kernel_i in permutation_set:
                 for dim in range(input_dim):
@@ -59,20 +79,19 @@ class FeatureMaps(ABC):
                                                     self.vectors[kernel_i[dim]].reshape(shape), 
                                                     mode = "same")
                 j += 1
+        return maps
+        
+    def get_features(self, x, window_size: int = 15, preprocess: bool = True, 
+        merge_symmetric: bool = True, compute_energy: bool = True):
+        input_dim = x.ndim
+        if preprocess:
+            x = self.preprocess_input(x, window_size, input_dim)
+        permutations = self.generate_kernel_permutations(input_dim)
+        maps = self.compute_maps_separably(x, input_dim, permutations)
         if merge_symmetric:
-            i = 0
-            j = 0
-            for permutation_set in permutations:
-                n_perm = len(permutation_set)
-                maps[...,j] = np.mean(maps[..., i:i+n_perm], axis = -1)
-                i += n_perm
-                j += 1
-            assert j == len(kernels_i)-1
-            maps = maps[...,:j]
+            maps = self.merge_symmetric_maps(maps, permutations)
         if compute_energy:
-            maps     = np.abs(maps)
-            abs_save = np.ones((window_size,)*input_dim) # absolute save
-            maps     = np.stack([signal.fftconvolve(maps[...,i], abs_save, mode = "valid") for i in range(maps.shape[-1])], axis = -1)
+            maps = self.compute_energy_maps(maps, window_size, input_dim)
         return maps
         
         
