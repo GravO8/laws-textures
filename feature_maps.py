6566785928ae1b231677481b1f2474a17b943e45
GravO8 <goncalo.valente.de.oliveira@gmail.com>
2022-07-14 11:52:23 +0100

    Generalize the `get_features` method

--- feature_maps.py
+++ feature_maps.py
@@ -1,5 +1,4 @@
-import numpy as np
-from itertools import combinations_with_replacement
+import numpy as np, itertools
 from scipy import signal
 
 VECTOR_INITIALS = ["L", "E", "S", "W", "R", "O"]
@@ -30,7 +29,7 @@ class FeatureMaps:
     def generate_vectors(self):
         N            = 1 + (self.vector_dims - 3) // 2
         j            = 0
-        combinations = list(combinations_with_replacement(range(len(self.basis_vectors)), N))
+        combinations = list(itertools.combinations_with_replacement(range(len(self.basis_vectors)), N))
         # self.vectors = {}
         self.vectors = []
         for comb in combinations:
@@ -41,18 +40,37 @@ class FeatureMaps:
             self.vectors.append(vector)
             j += 1
             
+    def get_kernel_shape(self, input_dim, dim):
+        return tuple([self.vector_dims if i == dim else 1 for i in range(input_dim)])
+            
     def get_features(self, x, preprocess: bool = True, merge_symmetric: bool = True):
         input_dim = x.ndim
-        kernels_i = list(combinations_with_replacement(range(self.vector_dims), input_dim))
-        kernels_i = kernels_i[1:] # remove the first non zero sum kernel
-        maps      = np.repeat(x[..., np.newaxis], len(kernels_i), axis = input_dim)
+        kernels_i = list(itertools.combinations_with_replacement(range(self.vector_dims+1), input_dim))
+        n_maps    = len(self.vectors)**(input_dim) -1
+        permutations = []
+        for kernel_i in kernels_i:
+            permutations.append( list(set(itertools.permutations(kernel_i, input_dim))) )
+        permutations.pop(0) # remove the first non zero sum kernel
         # if preprocess:
             # TODO
+        maps = np.repeat(x[..., np.newaxis], n_maps, axis = input_dim)
         for dim in range(input_dim):
-            shape = tuple([self.vector_dims if i == dim else 1 for i in range(input_dim)])
-            for kernel_i in kernels_i:
-                i = kernel_i[dim]
-                maps[..., dim] = signal.fftconvolve(maps[..., dim], self.vectors[i].reshape(shape), mode = "same")
+            shape = self.get_kernel_shape(input_dim, dim)
+            for permutation_set in permutations:
+                for kernel_i in permutation_set: 
+                    i = kernel_i[dim]
+                    maps[..., dim] = signal.fftconvolve(maps[..., dim], self.vectors[i].reshape(shape), mode = "same")            
+        if merge_symmetric:
+            feature_indexes = []
+            i = 0
+            j = 0
+            for permutation_set in permutations:
+                n_perm = len(permutation_set)
+                maps[...,j] = maps[..., i:i+n_perm].mean()
+                i += n_perm
+                j += 1
+            assert j == len(kernels_i)-1
+            maps = maps[...,:j]
         return maps
 
 def laws_textures(vector_dims: int = 5):
@@ -65,6 +83,6 @@ def laws_textures(vector_dims: int = 5):
 
 
 if __name__ == "__main__":
-    laws = laws_textures(vector_dims = 3)
-    x = np.zeros((32,32))
+    laws = laws_textures(vector_dims = 5)
+    x = np.zeros((32,32,32))
     laws.get_features(x)
